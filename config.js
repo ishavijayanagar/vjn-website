f9c767b0b87c89cdcb7e1cad569e67296240a51a
ishavijayanagar <ishaplacevjn@gmail.com>
2026-04-20 11:58:00 +0530

    made some changes. Fixed image preview from drive link.

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // after deploying (see setup-guide.md for how)
 // =============================================
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx-7y0kcp-0NMnoHj--GP34XzY_Ku1Y-tiJ7Q2W-xhuzSpTWEzfzsdzUmTvr0i3Ea8o/exec";
+const APPS_SCRIPT_URL = "YOUR_APPS_SCRIPT_URL_HERE";
