5f11f51cdcc05a835a61a47420fb29bf4be3b4d5
ishavijayanagar <ishaplacevjn@gmail.com>
2026-04-20 10:42:38 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+// =============================================
+// STEP 1: Paste your Google Apps Script URL here
+// after deploying (see setup-guide.md for how)
+// =============================================
+
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx-7y0kcp-0NMnoHj--GP34XzY_Ku1Y-tiJ7Q2W-xhuzSpTWEzfzsdzUmTvr0i3Ea8o/exec";
